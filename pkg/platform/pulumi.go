22aa665a8984a3f7c64423f8309ad00e457a72f9
Thomas Stromberg <t+github@chainguard.dev>
2023-08-29 14:09:56 -0400

    Add missing pulumi.go file

---
+++ pkg/platform/pulumi.go
@@ -0,0 +1,54 @@
+package platform
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// pulumiPeople parses the HTML output of the pulumi People page.
+type pulumiPeople struct{}
+
+func (p *pulumiPeople) Description() ProcessorDescription {
+	return ProcessorDescription{
+		Kind: "pulumi",
+		Name: "pulumi Site Permissions",
+		Steps: []string{
+			"Open https://pulumi.com/",
+			"Select your company/team",
+			"Click 'Settings'",
+			"Click 'People'",
+			"Save this page (Complete)",
+			"Collect resulting .html file for analysis (the other files are not necessary)",
+			"Execute 'yacls --kind={{.Kind}} --input={{.Path}}'",
+		},
+		MatchingFilename: regexp.MustCompile(`Pulumi.*.html$`),
+	}
+}
+
+func (p *pulumiPeople) Process(c Config) (*Artifact, error) {
+	src, err := NewSourceFromConfig(c, p)
+	if err != nil {
+		return nil, fmt.Errorf("source: %w", err)
+	}
+	a := &Artifact{Metadata: src}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(src.content))
+	if err != nil {
+		return nil, fmt.Errorf("document: %w", err)
+	}
+
+	// Find the People
+	doc.Find(".cdk-row").Each(func(i int, s *goquery.Selection) {
+		email := strings.TrimSpace(s.Find("p.login").Text())
+		name := strings.TrimSpace(s.Find("p.name").Text())
+		role := strings.TrimSpace(s.Find("span.ng-star-inserted").Text())
+		a.Users = append(a.Users, User{Account: email, Name: name, Role: role})
+	})
+
+	return a, nil
+}
